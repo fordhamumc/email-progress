60dd59578efe0544526043fe4ac6fd4e3c964709
Michael Foley <michaeldfoley@gmail.com>
2017-04-26 15:26:21 -0400

    replaces hard-coded aws bucket with variable

--- app.rb
+++ app.rb
@@ -5,16 +5,19 @@ configure do
   set :static_cache_control, [:no_cache, :no_store, :must_revalidate, max_age: 0]
 end
 
+
 get '/' do
+  @bucket = ENV.fetch('AWS_BUCKET')
   erb :index
 end
 
 get '/dashboard.html' do
+  @bucket = ENV.fetch('AWS_BUCKET')
   erb :dashboard
 end
 
-
 get '/progress.html' do
+  @bucket = ENV.fetch('AWS_BUCKET')
   <<-HTML
     <!DOCTYPE HTML>
     <html lang="en-US">
@@ -22,10 +25,10 @@ get '/progress.html' do
       <meta charset="UTF-8">
     </head>
     <body>
-      <img src="https://s3.amazonaws.com/fordham-givingday/progress-bar.png" width="630"><br />
-      <img src="https://s3.amazonaws.com/fordham-givingday/stats.png" width="440"><br />
-      <img src="https://s3.amazonaws.com/fordham-givingday/top-years.png" width="630"><br />
-      <img src="https://s3.amazonaws.com/fordham-givingday/top-areas.png" width="630">
+      <img src="https://s3.amazonaws.com/#{@bucket}/progress-bar.png" width="630"><br />
+      <img src="https://s3.amazonaws.com/#{@bucket}/stats.png" width="440"><br />
+      <img src="https://s3.amazonaws.com/#{@bucket}/top-years.png" width="630"><br />
+      <img src="https://s3.amazonaws.com/#{@bucket}/top-areas.png" width="630">
     </body>
     </html>
   HTML
